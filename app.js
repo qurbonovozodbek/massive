d6ef5faba66ca4a006a012dcc23736c3ab84fb37
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-21 17:14:06 +0500

    done masala

---
+++ app.js
@@ -0,0 +1,71 @@
+// let arr = [1, 2, 3, 4, 5]
+// function SumArr(arg) {
+//     let sum = 0;
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 0) {
+//             sum = sum + arg[i];
+//         }
+//     }
+//     return sum;
+// }
+// console.log(SumArr(arr));
+
+// 1-masala
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+// function SumArr(arg) {
+//     let sum = 0;
+//     for(let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 1) {
+//             sum = sum += 1;
+//         }
+//     }
+//     return sum;
+// }
+// console.log(SumArr(arr));
+
+
+// 2 - masala
+
+// let arr = [3, 4, 5, 6, 7, 10, 14]
+// function SumArr(arg) {
+//     let sum = 1;
+//     for(let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 3 == 0 || arg[i] % 7 == 0) {
+//             sum = sum *= arg[i];
+//         }
+//     }
+//     return sum;
+// }
+// console.log(SumArr(arr));
+
+// 3 - masala
+
+// let arr = [1, 2, 3, 5]
+// let rra = [2, "salom"];
+// function SumArr(arg) {
+//     // let max;
+//     for(let i = 0; i < arg.length; i++) {
+//         if (arg[i] % 2 == 1) {
+//             rra.push(arg[i]);
+//         }
+//     }
+//     // return max;
+// }
+// // rra.push(SumArr(arr));
+// console.log(rra);
+
+// 5 - masala
+
+// function SumArr(words) {
+//     let sum = [];
+//     for(let i = 0; i < words.length; i++) {
+//         som = words[i][0];
+//         sum.push(som);
+//     }
+//     return sum;
+// }
+
+// let word = ['for', 'while', 'DoWhile', 'function', 'array'];
+// let result = SumArr(word);
+// console.log(result);
